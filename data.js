b84bb17a2db3f32a997192457388a6c3d4d16989
Andrewij21 <andrewijaya988@gmail.com>
2023-01-27 12:56:51 +0700

    Fetch data from flickr

--- data.js
+++ data.js
@@ -1,19 +1,25 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import fetch from "node-fetch";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Listening on PORT:${PORT}`));
 app.use(cors({ origin: true }));
 
-app.get("/", (req, res) => {
-  res.json({
-    response: 200,
-    data: {
-      namaBarang: "meja",
-      harga: 2000,
-    },
-  });
+async function flickr() {
+  const response = await fetch(
+    "https://www.flickr.com/services/feeds/photos_public.gne?format=json"
+  );
+  // console.log({ res: await response });
+  const data = await response.text();
+  return JSON.parse(data.slice(15, -1));
+}
+
+app.get("/", async (req, res) => {
+  const data = await flickr();
+  // console.log({ data });
+  res.send(data);
 });
 //testing
 
